dddd04c80feaac5fc6048b23d54eb1db01dc4bac
Seyed <Seyed.etemadi@gmail.com>
2025-12-14 18:29:14 +0330

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// popup
+
+const popup = document.querySelector("#popup");
+
+setTimeout(() => {
+  popup.classList.add("popup");
+}, 3000);
+
+function closePopup() {
+  popup.classList.remove("popup");
+}
+
+popup.addEventListener("click", closePopup);
+
+// Burger Menu
+
+const burger = document.querySelector("#burger");
+
+function menu() {
+  const navItem = document.querySelectorAll(".nav-item");
+
+  for (const navItem of navItems) {
+    if (navItem.classList.contains("nav-item.show")) {
+      navItem.classList.remove("nav-item-show");
+    } else {
+      navItem.ClassList.add("nav-item-show");
+    }
+  }
+}
